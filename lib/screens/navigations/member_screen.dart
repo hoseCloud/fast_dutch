198ee80e3d0a328fbb611cc88141c821f010995c
hosecloud <4osecloud@gmail.com>
2023-02-06 16:18:19 +0900

    Add local_DB with models

--- lib/screens/navigations/member_screen.dart
+++ lib/screens/navigations/member_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
+import 'package:fast_dutch/models/member_model.dart';
 import 'package:fast_dutch/screens/add_models/add_member_screen.dart';
+import 'package:fast_dutch/widgets/member_widget.dart';
 import 'package:fast_dutch/widgets/navigation_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MemberScreen extends StatefulWidget {
   const MemberScreen({super.key});
@@ -10,6 +15,16 @@ class MemberScreen extends StatefulWidget {
 }
 
 class _MemberScreenState extends State<MemberScreen> {
+  late final SharedPreferences prefs;
+  late final List<String>? members;
+  final List<Widget> memberWidgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    initMembers();
+  }
+
   void onTapAddMember() {
     Navigator.push(
       context,
@@ -19,13 +34,27 @@ class _MemberScreenState extends State<MemberScreen> {
     );
   }
 
+  void initMembers() async {
+    prefs = await SharedPreferences.getInstance();
+    members = prefs.getStringList('member');
+
+    setState(() {
+      for (var member in members ?? []) {
+        var memberModel = MemberModel.fromJson(jsonDecode(member));
+        memberWidgets.add(Member(
+          memberModel: memberModel,
+        ));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Navigation(
       emptyMsg: 'No any member!',
       emptyButtonMsg: 'Add member',
       addButtonMsg: 'Add member',
-      widgets: const [],
+      widgets: memberWidgets,
       onTapAddFunc: onTapAddMember,
       onTapEmptyFunc: onTapAddMember,
     );
